98cc29f6d83b8f7e8b3b676f7c6fd79558dd1d00
angeldmolina <theangeldmolina@gmail.com>
2023-06-13 15:21:51 -0400

    updated models

--- models/models.go
+++ models/models.go
@@ -7,19 +7,38 @@ import (
 
 type Author struct {
 	gorm.Model
-	ID      uint      `json:"id" gorm:"primaryKey`
-	Name    string      `json:"name" gorm:"text;not null`
+	ID      uint      `json:"id" gorm:"primaryKey"`
+	Name    string      `json:"name" gorm:"text;not null"`
 
-	Posts []Post `json: "posts"`
+	Posts []Post `json:"posts"`
 }
 
 type Post struct {
 	gorm.Model
-	ID        uint  `json:"id" gorm:"primaryKey`
-	Title     string  `json:"title" gorm:"text;not null;<-:update`
-	Content   string  `json:"content" gorm:"text;not null;<-:update`
-	CreatedAt time.Time `json: "publishedAt"`
-  	UpdatedAt time.Time `json: "updatedAt"`
+	ID        uint  `json:"id" gorm:"primaryKey"`
+	Title     string  `json:"title" gorm:"text;not null;<-:update"`
+	Content   string  `json:"content" gorm:"text;not null;<-:update"`
+	CreatedAt time.Time `json:"publishedAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 
-	Author Author `json: "author"`
+	Author Author `json:"author"`
+}
+
+type Category struct {
+	gorm.Model
+	ID   uint   `json:"id" gorm:"primaryKey"`
+	Name string `json:"name" gorm:"text;not null"`
+
+	Posts []Post `json:"posts"`
+}
+
+type Comment struct {
+	gorm.Model
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Content   string    `json:"content" gorm:"text;not null"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+
+	Post   Post   `json:"post"`
+	Author Author `json:"author"`
 }
